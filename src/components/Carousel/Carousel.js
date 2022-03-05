f40411a0005a415c0a81f46aea9465771cec84dd
David <zhangdavid5915@hotmail.com.au>
2022-03-05 14:25:48 +1100

    Have a functioning carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Carousel.css'
-import ItemOne from './CarouselImages/test.jpg'
+import { images } from './Helper'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
 
-export const images = [
-  {title: "Item 1", subtitle: "Description of Item 1", img: ItemOne},
-];
+/*
+Youtube link: https://www.youtube.com/watch?v=iyj3TZXg2gQ
+*/
 
 const Carousel = () => {
+
+  const [currImg, setCurrImg] = useState(0)
+
   return (
     <div className='slide'>
-      <div className='slideInner'>
-        <img src={images[0].img}
+      <div 
+        className='slideInner' 
+        style={{ backgroundImage: `url(${images[currImg].img})` }}
+      >
+        <div 
+          className="left" 
+          onClick={() => {
+            currImg > 0 && setCurrImg(currImg - 1)
+          }}
+        >
+          <ArrowBackIosIcon style={{fontSize: 30}} />
+        </div>
+        <div className="center"></div>
+        <div 
+          className="right"
+          onClick={() => {
+            currImg < images.length - 1 && setCurrImg(currImg + 1)
+          }}
+        >
+          <ArrowForwardIosIcon style={{fontSize: 30}}/>
+        </div>
+
       </div>
     </div>
   )
---
+++ src/components/Carousel/Helper.js
@@ -0,0 +1,23 @@
+import UAC1 from './CarouselImages/UAC1.jpg'
+import UAC2 from './CarouselImages/UAC2.jpg'
+import UAC3 from './CarouselImages/UAC3.png'
+import CI from './CarouselImages/CI.png'
+import SFA1 from './CarouselImages/SFA1.png'
+import SFA2 from './CarouselImages/SFA2.jpg'
+import D1 from './CarouselImages/D1.jpg'
+import D2 from './CarouselImages/D2.jpg'
+import D3 from './CarouselImages/D3.jpg'
+import YECC from './CarouselImages/YECC.png'
+
+export const images = [
+    {title: "Item 1", subtitle: "Description of Item 1", img: UAC1},
+    {title: "Item 2", subtitle: "Description of Item 2", img: UAC2},
+    {title: "Item 3", subtitle: "Description of Item 3", img: UAC3},
+    {title: "Item 4", subtitle: "Description of Item 3", img: CI},
+    {title: "Item 5", subtitle: "Description of Item 3", img: SFA1},
+    {title: "Item 6", subtitle: "Description of Item 3", img: SFA2},
+    {title: "Item 7", subtitle: "Description of Item 3", img: D1},
+    {title: "Item 8", subtitle: "Description of Item 3", img: D2},
+    {title: "Item 9", subtitle: "Description of Item 3", img: D3},
+    {title: "Item 10", subtitle: "Description of Item 3", img: YECC},
+];

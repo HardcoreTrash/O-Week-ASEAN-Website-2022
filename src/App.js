222972ccd5e07f20cc6fe7f1653d3cd968085f1b
David <zhangdavid5915@hotmail.com.au>
2022-03-12 14:18:05 +1100

    Added an empty profile template

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import UAC_header from './components/Images/UAC2021.png'
 import joinUs from './components/Images/Joinus.jpg'
 import FirstSection from './components/FirstSection'
 import Profile from './components/Profile';
+import Flyer from './components/Images/1.1.png'
 
 function App() {
   return (
@@ -22,11 +23,9 @@ function App() {
       />
 
       <Section title='Previous Events' sectionNo='2' content={<Carousel/>}/>
-
       <Profile title='Meet Our Team' />
-
       <Section title='Recruitment' content='' sectionNo='2' backImage={joinUs} style={{ backgroundImage: `url(${joinUs})`, backgroundRepeat: 'no-repeat', height: '100vh', width: '100vw'}}/>
-      <Section title='O-Week Flyers' content=''/>
+      <Section title='O-Week Flyers' content={<img src={Flyer}/>}/>
       <Section title='YECC' content={<img src={YECC_image}/>} sectionNo='2'/>
       <Section title='UNSW ASEAN Conference 2021' content={<img src={UAC_header} className='UAC_css'/>}/>
 

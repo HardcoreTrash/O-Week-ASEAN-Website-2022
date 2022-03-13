3ccd994db88b55cc8a2dc22c67ca775e00ae58a3
David <zhangdavid5915@hotmail.com.au>
2022-03-14 01:24:55 +1100

    Changed font size to make it more readable

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Section from './components/Section'
 import YoutubeEmbed from "./components/YoutubeEmbed";
 import YECC_image from './components/Images/Presentation1.png'
 import UAC_header from './components/Images/UAC2021.png'
-import joinUs from './components/Images/Joinus.jpg'
 import FirstSection from './components/FirstSection'
 import Profile from './components/Profile';
 import Flyer from './components/Images/1.1.png'
@@ -25,12 +24,16 @@ function App() {
 
       <Section title='Previous Events' sectionNo='2' content={<Carousel/>}/>
       <Profile title='Meet Our Team' />
-      {/* <Section title='Recruitment' content='' sectionNo='2' backImage={joinUs} style={{ backgroundImage: `url(${joinUs})`, backgroundRepeat: 'no-repeat', height: '100vh', width: '100vw'}}/> */}
       <Recruitment title='Recruitment' />
 
       <Section title='O-Week Flyers' content={<img src={Flyer}/>}/>
       <Section title='YECC' content={<img src={YECC_image}/>} sectionNo='2'/>
-      <Section title='UNSW ASEAN Conference 2021' content={<img src={UAC_header} className='UAC_css'/>}/>
+      <Section title='UNSW ASEAN Conference 2021' 
+      content={
+        <img src={UAC_header} className='UAC_css'/>
+      }
+      sectionNo='3'
+      />
 
     </div>
   );
--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './Section.css'
+import Button from '../Header/Button'
 
-function Section({title, content, sectionNo=1, backImage}) {
+const onClick = (link) => {
+  return function() {
+      window.open(link);
+  } 
+}
+
+function Section({title, content, sectionNo=1}) {
     if (sectionNo == 1) {
         return (
             <div className='first_back'>
@@ -11,12 +18,22 @@ function Section({title, content, sectionNo=1, backImage}) {
             </div>
         )
     }
-    else {
+    else if (sectionNo == 2) {
         return (
             <div className='second_back'>
-                {/* <div style={{ backgroundImage: `url(${backImage})`, backgroundRepeat: 'no-repeat', height: '100vh', width: '100vw'}}/> */}
                 <h1 className='second_title'> {title} </h1>
                 <div className='content_centre'> {content} </div>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className='first_back'>
+                <h1 className='first_title'> {title} </h1>
+                <div className='content_centre'> {content} </div>
+                <div className='UAC_button'>
+                    <Button text='Learn More' onClick={onClick('https://www.unswaseanconference.com/')}/>
+                </div>
             </div>
         )
     }

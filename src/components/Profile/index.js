14c96c80b4818d235af52a5d15d17a3bb494bce7
David <zhangdavid5915@hotmail.com.au>
2022-03-26 04:07:55 +1100

    Changed the Meet our Team section into the actual thing

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,22 +2,46 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Profile.css'
 
-function Profile({title, content}) {
+const ProfileBox = ({pic, title}) => { 
+    return <div className='profile_info'>
+        <img src={pic} className='profile_pic'/>
+        <p className='nameStyle'> {title} </p>
+    </div>
+}
+
+function Profile({title}) {
     return (
         <div className='back_colour'>
             <h1 className='first_title'> {title} </h1>
-            <div className='six_pro'>
-                <div className='pro_1'>
-                    <p>Danielle </p>
+
+            <div className='pro_1'>
+                {/* <img src={`${process.env.PUBLIC_URL}/Profile/Dani.png`} className='profile_pic'/>
+                <img src={`${process.env.PUBLIC_URL}/Profile/Arthur.png`} className='profile_pic'/>
+                <img src={`${process.env.PUBLIC_URL}/Profile/Imandi.png`} className='profile_pic'/> */}
+
+                <ProfileBox pic={`${process.env.PUBLIC_URL}/Profile/Dani.png`} title='Danielle' />
+                <ProfileBox pic={`${process.env.PUBLIC_URL}/Profile/Arthur.png`} title='Danielle' />
+                <ProfileBox pic={`${process.env.PUBLIC_URL}/Profile/Imandi.png`} title='Danielle' />
+
+            </div>
+
+            <div className='pro_2'>
+                {/* <img src={`${process.env.PUBLIC_URL}/Profile/Eric.png`} className='profile_pic'/>
+                <img src={`${process.env.PUBLIC_URL}/Profile/David_S.png`} className='profile_pic'/>
+                <img src={`${process.env.PUBLIC_URL}/Profile/Lesley.png`} className='profile_pic'/> */}
+
+                <ProfileBox pic={`${process.env.PUBLIC_URL}/Profile/Eric.png`} title='Danielle' />
+                <ProfileBox pic={`${process.env.PUBLIC_URL}/Profile/David_S.png`} title='Danielle' />
+                <ProfileBox pic={`${process.env.PUBLIC_URL}/Profile/Lesley.png`} title='Danielle' />
+
+            </div>
+
+{/* <p>Danielle </p>
                     <p>Arthur </p>
                     <p>Imandi </p>
-                </div>
-                <div className='pro_2'>
                     <p>Eric </p>
                     <p>David </p>
-                    <p>Lesley </p>
-                </div>
-            </div>
+                    <p>Lesley </p> */}
         </div>
     )
 }
